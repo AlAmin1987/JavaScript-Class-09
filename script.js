a2a56e627280935a36807b16b61ceb815a15de63
Md.AlAmin <alamin.dreams2021@gmail.com>
2023-01-07 15:57:33 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// getElementById Method
+// const heading = document.getElementById("IdOne");
+// console.log(heading.innerHTML);
+// heading.innerHTML = "We Learn JavaScript With Fun";
+
+// getElementByTagName Method
+
+// const tag1 = document.getElementsByTagName("h2")[0];
+// console.log(tag1.innerText);
+// tag1.innerText = "We Learn JavaScript With Fun ";
+
+// getElementByClassName
+
+// const ClassOne = document.getElementsByClassName("One")[0];
+// console.log(ClassOne.innerHTML);
+// ClassOne.innerHTML = "Doccument Object Model";
+// console.log(ClassOne.innerHTML);
+
+// DOM Element Style
+
+const Identification = document.getElementById("IDOne");
+console.log(Identification.innerText);
+IDOne.style.color = "red";
+IDOne.style.fontSize = "45px";
+IDOne.style.border = "1px solid blue";
+IDOne.style.textAlign = "Center";
+IDOne.style.backgroundColor = "Green";
+
+// Attributes
+// const Identification = document.getElementById("IDOne");
+// console.log(Identification.innerText);
+// console.log(IDOne.attributes);
